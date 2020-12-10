5e59a7b4d9e9a5af064ef3d84ed1fe9a82876974
hyejungg <68772751+hyejungg@users.noreply.github.com>
2020-12-10 22:54:54 +0900

    test try.php

---
+++ try.php
@@ -0,0 +1,19 @@
+<?php
+
+$db_host = "localhost";
+
+$db_user = "dbpt10";
+
+$db_passwd = "dbpadmint10!";
+
+$db_name = "dbp";
+
+$conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
+
+
+
+if (mysqli_connect_errno($conn)) {
+    echo "데이터베이스 연결 실패: " . mysqli_connect_error();
+} else {
+    echo "데이터베이스 연결 성공";
+}
